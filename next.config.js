de6dfd9570fa040b7f3a9ac9c80da1c5aade1617
schoolourauto-rgb <school.ourauto@gmail.com>
2026-02-11 11:44:40 +0000

    fix: clean duplicate nextConfig declaration

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
 module.exports = nextConfig;
-
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
-
-module.exports = withBundleAnalyzer(nextConfig);
