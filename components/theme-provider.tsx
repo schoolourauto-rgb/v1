8a65c9fbc9678de4c7d7e626bc2d1763c48966fe
schoolourauto-rgb <school.ourauto@gmail.com>
2026-02-12 09:10:04 +0000

    Enable system theme detection and persistent theme storage

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,14 +1,18 @@
 "use client"
 
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { ReactNode } from "react"
 
-export function ThemeProvider({ children }: { children: ReactNode }) {
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { ThemeProviderProps } from "next-themes/dist/types"
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="dark"
-      enableSystem={false}
+      defaultTheme="system"
+      enableSystem={true}
+      storageKey="ourauto-theme"
+      disableTransitionOnChange={false}
+      {...props}
     >
       {children}
     </NextThemesProvider>
