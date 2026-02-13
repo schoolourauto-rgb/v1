c3d06b1e9c4cb8f50d71828f63fc10bc69beb2e4
schoolourauto-rgb <school.ourauto@gmail.com>
2026-02-13 04:56:28 +0000

    fix: properly mount CarPasteGenerate component

--- app/dealer/add-car/page.tsx
+++ app/dealer/add-car/page.tsx
@@ -4,4 +4,12 @@ import CarPasteGenerate from "./CarPasteGenerate";
 
 export default function AddCarPage() {
   return <CarPasteGenerate />;
-}
+  "use client";
+
+  export default function AddCarPage() {
+    return (
+      <div className="p-6">
+        <CarPasteGenerate />
+      </div>
+    );
+  }
